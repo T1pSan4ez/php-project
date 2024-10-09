68c085e29b59e775ec5337107fe5a35cec651ab6
Alex <sashaglotov2001@gmail.com>
2024-10-09 11:46:43 +0000

    first

---
+++ src/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$username = 'palmo';
+$password = 'palmo';
+$dbname = 'palmo';
+$host = 'mysql';
+
+$dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8;';
+
+try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    var_dump($conn);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
